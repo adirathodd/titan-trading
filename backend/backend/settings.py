f7fa5cd2d9bcd670cdc3bff33bd6a3f42ad6210a
adirathodd <88114611+adirathodd@users.noreply.github.com>
2024-11-10 00:13:52 -0500

    Working with render backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'titanapi.onrender.com',
-    'localhost'
+    'localhost',
+    '127.0.0.1'
 ]
 
 # Application definition
--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'https://titanapi.onrender.com/api/',
 });
 
 axiosInstance.interceptors.request.use(
