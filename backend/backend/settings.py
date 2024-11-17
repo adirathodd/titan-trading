41c2c6026e3928071f54aa64b29153a45b0d2f53
adirathodd <88114611+adirathodd@users.noreply.github.com>
2024-11-17 03:37:34 -0500

    Allow all origins

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -56,6 +56,8 @@ CORS_ALLOWED_ORIGINS = [
     'https://adirathodd.github.io'
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
